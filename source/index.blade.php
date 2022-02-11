5e684e2a6413e9c45c6a78385493f50260168f4b
DawoodEmran <rockdawoodde@gmail.com>
2022-02-11 21:48:39 +0330

    add new changies

--- source/index.blade.php
+++ source/index.blade.php
@@ -3,5 +3,7 @@
 @section('body')
 <div class="p-8">
     <h1 class="text-3xl font-bold bg-slate-400">Hello world!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia excepturi sit sed eos laboriosam quisquam hic
+        quis. Quasi rem dignissimos minima nobis impedit, eius commodi adipisci obcaecati quo totam quaerat?</p>
 </div>
 @endsection
